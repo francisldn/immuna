c23d3d5b1a2b80e93453994fba89ca89d9f6795e
Francis_ldn <fnhh04@gmail.com>
2022-11-21 20:05:49 +0000

    add drop-shadow

--- src/components/LoginHeader.tsx
+++ src/components/LoginHeader.tsx
@@ -3,7 +3,7 @@ import Brand from "./Brand"
 
 export default function LoginHeader() {
   return (
-    <header className='flex justify-between mx-[4rem] pt-4 pb-4 items-center'>
+    <header className='flex justify-between mx-[4rem] pt-4 pb-4 items-center shadow-[0px_1px_10px_0px_rgba(132,132,132,0.07)]'>
       <Brand width={"w-[7rem]"} height={"h-[1.625rem]"} />
       <div className='flex gap-2 items-center'>
         <div className='rounded-full w-8 h-8 bg-[#D1D1D1]'></div>
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar"
 
 export default function PageHeader() {
   return (
-    <header className='flex pt-4 pb-4 pl-[1.25rem] header-width items-center pr-[1.25rem]'>
+    <header className='flex pt-4 pb-4 pl-[1.25rem] header-width items-center pr-[1.25rem] shadow-[0px_1px_10px_0px_rgba(74,74,74,0.07)]'>
       <div className='flex flex-1 items-center'>
         <h1 className='text-lg font-[500] pr-[3.375rem]'>Dashboard</h1>
         <div>
